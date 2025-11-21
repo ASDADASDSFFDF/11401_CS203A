212a673835a2438b0c6c4dba62fbac260bb3567d
張景翔 <ben950810@gmail.com>
2025-11-21 12:36:18 +0800

    Update hash_fn.c

--- Assignment/C/hash_fn.c
+++ Assignment/C/hash_fn.c
@@ -17,7 +17,7 @@
 
 int myHashInt(int key, int m) {
     // TODO: replace with your own design
-    return key % m;  // division method example
+    return (key % m + m) % m;  // division method example
 }
 
 int myHashString(const char* str, int m) {
